21bf5f44f8087bf62c41bed382a2e42c7b05c3a6
Leonardo Ossino <leonardo.ossino@studio.unibo.it>
2025-08-07 11:48:29 +0200

    Adjustment done to Fraction class (may be complete)

--- fraction.py
+++ fraction.py
@@ -3,35 +3,37 @@ import math
 
 
 class Fraction:
-    def __init__(self, numerator: int | float = 0, denominator: int = 1):
+    def __init__(self, numerator: int | float | str = 0, denominator: int = 1):
         if denominator == 0:
-            raise ValueError("Error: division by zero.")
-        self.num = int(numerator)
-        power_of_ten: int = 1
-        while self.num != numerator:
-            numerator *= 10
-            power_of_ten = power_of_ten * 10
+            raise ZeroDivisionError("Error raised by Fraction class' constructor")
+        if isinstance(numerator, str):
+            f = Fraction.from_str(numerator)
+            self.num = f.num
+            self.den = f.den
+        else:
             self.num = int(numerator)
-        self.den = power_of_ten * denominator
-        self.simplify()
+            power_of_ten: int = 1
+            while self.num != numerator:
+                numerator *= 10
+                power_of_ten = power_of_ten * 10
+                self.num = int(numerator)
+            self.den = power_of_ten * denominator
+            self.simplify()
 
     def simplify(self):
-        gcd = math.gcd(self.num, self.den)
-        self.num //= gcd
-        self.den //= gcd
+        greatest_common_divisor = math.gcd(self.num, self.den)
+        self.num //= greatest_common_divisor
+        self.den //= greatest_common_divisor
         if self.den < 0:  # Ensure denominator is positive
             self.num *= -1
             self.den *= -1
 
-    def set_fr(self, numerator: int, denominator: int):
-        if denominator == 0:
-            raise ValueError("Error: division by zero.")
-        self.num = numerator
-        self.den = denominator
-        self.simplify()
-
-    def get_num(self) -> int:
-        return self.num
+    @classmethod
+    def from_str(cls, fraction_as_string: str) -> "Fraction":
+        numerator, denominator = fraction_as_string.split("/")
+        numerator = int(numerator)
+        denominator = int(denominator)
+        return Fraction(numerator, denominator)
 
     def __add__(self, other: "Fraction") -> "Fraction":
         num = self.num * other.den + other.num * self.den
@@ -50,7 +52,7 @@ class Fraction:
 
     def __truediv__(self, other: "Fraction") -> "Fraction":
         if other.num == 0:
-            raise ZeroDivisionError("Error: division by zero.")
+            raise ZeroDivisionError("Error raised by division operator")
         num = self.num * other.den
         den = self.den * other.num
         return Fraction(num, den)
@@ -60,7 +62,7 @@ class Fraction:
             return self.num * other.den == self.den * other.num
         elif isinstance(other, int):
             return self.num == self.den * other
-        return False
+        raise TypeError("Comparasion between Fraction and invalid type")
 
     def __ne__(self, other: Union["Fraction", int]) -> bool:
         return not self.__eq__(other)
