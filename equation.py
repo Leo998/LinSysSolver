ef79f67ada5668726be761b014fb969e8675634f
Leonardo Ossino <leonardo.ossino@studio.unibo.it>
2025-08-13 11:42:29 +0200

    Added equation class and its tests

---
+++ equation.py
@@ -0,0 +1,87 @@
+import math
+
+from fraction import Fraction
+
+
+class Equation:
+
+
+    def __init__(self, coefficients: list[Fraction]):
+        self.coefficients: list[Fraction] = coefficients
+    
+    def __add__(self, other: "Equation") -> "Equation":
+        if len(self.coefficients) != len(other.coefficients):
+            raise ValueError("Equation instances have different number of coefficients")
+        new_coefficients: list[Fraction] = []
+        for coefficient1, coefficient2 in zip(self.coefficients, other.coefficients):
+            new_coefficients.append(coefficient1 + coefficient2)
+        return Equation(new_coefficients)
+    
+    def __sub__(self, other: "Equation") -> "Equation":
+        if len(self.coefficients) != len(other.coefficients):
+            raise ValueError("Equation instances have different number of coefficients")
+        new_coefficients: list[Fraction] = []
+        for coefficient1, coefficient2 in zip(self.coefficients, other.coefficients):
+            new_coefficients.append(coefficient1 - coefficient2)
+        return Equation(new_coefficients)
+    
+    def __mul__(self, other: "Fraction") -> "Equation":
+        new_coefficients: list[Fraction] = []
+        for coefficient in self.coefficients:
+            new_coefficients.append(coefficient * other)
+        return Equation(new_coefficients)
+    
+    def __truediv__(self, other: "Fraction") -> "Equation":
+        new_coefficients: list[Fraction] = []
+        for coefficient in self.coefficients:
+            new_coefficients.append(coefficient / other)
+        return Equation(new_coefficients)
+
+    def __eq__(self, other: "Equation") -> bool:
+        if len(self.coefficients) != len(other.coefficients):
+            raise ValueError("Equation instances have different number of coefficients")
+        for coefficient1, coefficient2 in zip(self.coefficients, other.coefficients):
+            if coefficient1 != coefficient2:
+                return False
+        return True
+
+    def __ne__(self, other: "Equation") -> bool:
+        return not self.__eq__(other)
+
+    def __str__(self):
+        output: list[str] = []
+        sign: str = ""
+        for subscript, coefficient in enumerate(self.coefficients[:-1], 1):
+            if coefficient.num < 0:
+                sign = "-"
+                coefficient = coefficient * Fraction(-1)
+            output.append(f"{sign} {coefficient} x{subscript} ")
+            sign = "+"
+        if self.coefficients[-1].num < 0:
+            sign = "-"
+            self.coefficients[-1] = self.coefficients[-1] * Fraction(-1)
+            output.append(f"{sign} {self.coefficients[-1]} = 0")
+            self.coefficients[-1] = self.coefficients[-1] * Fraction(-1)
+        else:
+            output.append(f"{sign} {self.coefficients[-1]} = 0")
+        output[0] = output[0].lstrip()
+        return "".join(output)
+
+    def __repr__(self):
+        output: list[str] = []
+        sign: str = ""
+        for subscript, coefficient in enumerate(self.coefficients[:-1], 1):
+            if coefficient.num < 0:
+                sign = "-"
+                coefficient = coefficient * Fraction(-1)
+            output.append(f"{sign} {coefficient} x{subscript} ")
+            sign = "+"
+        if self.coefficients[-1].num < 0:
+            sign = "-"
+            self.coefficients[-1] = self.coefficients[-1] * Fraction(-1)
+            output.append(f"{sign} {self.coefficients[-1]} = 0")
+            self.coefficients[-1] = self.coefficients[-1] * Fraction(-1)
+        else:
+            output.append(f"{sign} {self.coefficients[-1]} = 0")
+        output[0] = output[0].lstrip()
+        return "".join(output)
---
+++ test_equation.py
@@ -0,0 +1,63 @@
+import pytest
+
+from fraction import Fraction
+from equation import Equation
+
+def test_init():
+    e1 = Equation([Fraction(2, 4), Fraction(1), Fraction(2.8)])
+    assert e1.coefficients[0] == Fraction(1, 2)
+    assert e1.coefficients[1] == Fraction(-1, -1)
+    assert e1.coefficients[2] == Fraction(2.8, 1)
+    
+
+def test_add():
+    e1 = Equation([Fraction(2, 4), Fraction(1), Fraction(2.8)])
+    e2 = Equation([Fraction(1, 4), Fraction(-2), Fraction(2.3)])
+    e3 = Equation([Fraction(3, 4), Fraction(-1), Fraction(51, 10)])
+    e4 = Equation([Fraction(1, 4), Fraction(-2)])
+    assert e1 + e2 == e3
+    with pytest.raises(ValueError):
+        e1 + e4
+        
+def test_sub():
+    e1 = Equation([Fraction(2, 4), Fraction(1), Fraction(2.8)])
+    e2 = Equation([Fraction(1, 4), Fraction(-2), Fraction(2.3)])
+    e3 = Equation([Fraction(1, 4), Fraction(3), Fraction(1, 2)])
+    e4 = Equation([Fraction(1, 4), Fraction(-2)])
+    assert e1 - e2 == e3
+    with pytest.raises(ValueError):
+        e1 - e4
+
+def test_mul():
+    e1 = Equation([Fraction(2, 4), Fraction(1), Fraction(2.8)])
+    f1 = Fraction(1 , 2)
+    e3 = Equation([Fraction(1, 4), Fraction(0.5), Fraction(7, 5)])
+    assert e1 * f1 == e3
+
+def test_truediv():
+    e1 = Equation([Fraction(2, 4), Fraction(1), Fraction(2.8)])
+    f1 = Fraction(1 , 2)
+    e3 = Equation([Fraction(1), Fraction(2), Fraction(28, 5)])
+    assert e1 / f1 == e3
+
+def test_eq_ne():
+    e1 = Equation([Fraction(2, 4), Fraction(1), Fraction(2.8)])
+    e2 = Equation([Fraction(2, 4), Fraction(-1), Fraction(2.8)])
+    e3 = Equation([Fraction(1, 2), Fraction(5, 5), Fraction(5.6, 2)])
+    e4 = Equation([Fraction(1, 4), Fraction(-2)])
+    assert e1 == e3
+    assert e1 != e2
+    with pytest.raises(ValueError):
+        e1 == e4
+
+def test_str():
+    e1 = Equation([Fraction(1, 2), Fraction(5, 5), Fraction(5.4, -2)])
+    e2 = Equation([Fraction(-2, 5), Fraction(-1), Fraction(2.8)])
+    assert e1.__str__() == "1/2 x1 + 1 x2 - 27/10 = 0"
+    assert e2.__str__() == "- 2/5 x1 - 1 x2 + 14/5 = 0"
+
+def test_repr():
+    e1 = Equation([Fraction(1, 2), Fraction(5, 5), Fraction(5.4, -2)])
+    e2 = Equation([Fraction(-2, 5), Fraction(-1), Fraction(2.8)])
+    assert e1.__repr__() == "1/2 x1 + 1 x2 - 27/10 = 0"
+    assert e2.__repr__() == "- 2/5 x1 - 1 x2 + 14/5 = 0"
